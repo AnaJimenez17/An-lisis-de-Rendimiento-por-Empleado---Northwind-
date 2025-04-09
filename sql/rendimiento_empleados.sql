6e9f78ea397a22ace667017d48304a8edbda3f4d
AnaJimenez17 <Anajimenez8917@gmail.com>
2025-04-09 12:52:23 -0600

    Create rendimiento_empleados.sql
    Northwind S.A
    
    🔍 Análisis de Rendimiento por empleado 
    
    Este es un análisis de rendimiento por empleado de la empresa ficticia Northwind S.A. El proyecto tiene como objetivo evaluar el rendimiento de los empleados en función de ventas, ingresos, participación total y variación interanual.
    
    🛠 Primera etapa: SQL Server
    
    Para elaborar el análisis, se diseñó una consulta que relaciona las tablas; Products, Categories, Employees, Order Details y Orders. Se aplicaron funciones de agregación como SUM, COUNT y AVG, así como funciones de ventana con OVER() para calcular indicadores clave como:
     
    ✅ Ventas totales
    ✅ Ingreso promedio
    ✅  Participación en ventas
    ✅ Ingreso total por empleado
    ✅ Porcentaje de ingreso por empleado
    
    Se realizó un Top 10 de empleados con mayores ventas. Los datos fueron agrupados por nombre, apellido, categoría y fecha mediante GROUP BY, y filtrados con HAVING para mantener los registros relevantes. Se aplicó ORDER BY para ordenar de forma descendente según volumen de ventas. Finalmente, se creó una vista para facilitar la exportación a Power BI.

---
+++ sql/rendimiento_empleados.sql
@@ -0,0 +1,25 @@
+ALTER VIEW  Análisis_de_Rendimiento_x_Empleado  AS*/
+select top 10 O.OrderDate, E.FirstName+ ' '+ E.LastName as Empleado, 
+	C.CategoryName as Categoria,
+	count(O.OrderID) AS Orden_Total,
+	count(P.UnitsInStock) as  Stock_Disponible,
+	sum(OD.Quantity) AS  Venta_Total,
+    --Para calcular el porcentaje de ventas por empleados
+	Concat(CAST(ROUND(SUM(OD.Quantity) * 100.0 / SUM(SUM(OD.Quantity)) OVER (PARTITION BY E.EmployeeID), 2) AS DECIMAL(5, 2)), '%') AS Porcentaje_Venta,
+	
+    --Para calcular el ingreso promedio y promedio total
+	Concat('$',cast(ROUND(AVG(ISNULL(OD.UnitPrice, 0) * ISNULL(OD.Quantity, 0)), 2)  AS  DECIMAL(18,2))) AS  Ingreso_Promedio,
+	CONCAT('$',cast(ROUND(SUM(ISNULL(OD.UnitPrice, 0) * ISNULL(OD.Quantity, 0))   * (1 - ISNULL (OD.Discount,0)), 2) 	AS DECIMAL(18, 2))) AS  Ingreso_Total,
+    CONCAT(CAST(ROUND( SUM(OD.UnitPrice * OD.Quantity * (1 - OD.Discount)) * 100.0 / SUM(SUM(OD.UnitPrice * OD.Quantity * (1 - OD.Discount))) OVER (), 
+        2) AS DECIMAL(5, 2)), '%') AS P_Ingreso_x_Empleado
+
+	FROM  [Order Details] OD
+		 INNER JOIN Orders O ON O.OrderID = OD.OrderID
+		 INNER JOIN Employees E ON E.EmployeeID = O.EmployeeID
+		 INNER JOIN Products P ON P.ProductID = OD.ProductID
+		 INNER JOIN Categories C ON C.CategoryID = P.CategoryID
+		 GROUP BY O.OrderDate, E.FirstName, E.LastName, OD.Discount,
+		C.CategoryName, E.EmployeeID  HAVING sum(OD.Quantity)>= 50
+		ORDER BY Venta_Total DESC;
+
+ 
